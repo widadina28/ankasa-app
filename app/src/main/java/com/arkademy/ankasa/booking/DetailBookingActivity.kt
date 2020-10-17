6a2acd901af05d85aa5fb476be0aeffa8bcc576a
reihanbay <bung.reihan28@gmail.com>
2020-10-17 12:30:34 +0700

    pull-develop

---
+++ app/src/main/java/com/arkademy/ankasa/booking/DetailBookingActivity.kt
@@ -0,0 +1,12 @@
+package com.arkademy.ankasa.booking
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.arkademy.ankasa.R
+
+class DetailBookingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_booking)
+    }
+}
