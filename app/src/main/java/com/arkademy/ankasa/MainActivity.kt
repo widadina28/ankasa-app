02659644263859c88297f8c1dde40132cdb91343
ilham aliyudin <ilhampti3@gmail.com>
2020-10-17 23:45:03 +0700

    add search flight, mybooking, profile

--- app/src/main/java/com/arkademy/ankasa/MainActivity.kt
+++ app/src/main/java/com/arkademy/ankasa/MainActivity.kt
@@ -2,8 +2,14 @@ package com.arkademy.ankasa
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.arkademy.ankasa.dashboard.booking.BookingAdapter
+import com.arkademy.ankasa.dashboard.booking.BookingModel
+import kotlinx.android.synthetic.main.fragment_booking.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ app/src/main/java/com/arkademy/ankasa/dashboard/ExploreFragment.kt
@@ -0,0 +1,19 @@
+package com.arkademy.ankasa.dashboard
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.arkademy.ankasa.R
+
+class ExploreFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_explore, container, false)
+    }
+}
